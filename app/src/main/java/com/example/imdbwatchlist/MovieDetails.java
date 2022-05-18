d4eacdeddcd6e4fa0fc9fe8778c242402477f4b1
nbiba <mednbiba@gmail.com>
2022-05-18 20:16:01 +0100

    Search and Display : OK

--- app/src/main/java/com/example/imdbwatchlist/MovieDetails.java
+++ app/src/main/java/com/example/imdbwatchlist/MovieDetails.java
@@ -1,6 +1,7 @@
 package com.example.imdbwatchlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
@@ -12,9 +13,10 @@ import android.widget.Toast;
 import com.example.imdbwatchlist.Adapters.CastRecyclerAdapter;
 import com.example.imdbwatchlist.Listeners.OnDetailsApiLister;
 import com.example.imdbwatchlist.models.DetailApiResponse;
+import com.squareup.picasso.Picasso;
 
 public class MovieDetails extends AppCompatActivity {
-    TextView TextView_movieName,TextView_movieReleased,TextView_movieRuntime,TextView_movieRating,TextView_movieVotes,TextView_moviePlot;
+    TextView TextView_movieName,TextView_movieReleased,TextView_movieRuntime,TextView_movieRating,TextView_movieVotes,TextView_moviePlot,TextView_stars;
     ImageView ImageView_Poster2;
     RecyclerView recycler_view_cast;
     CastRecyclerAdapter adapter;
@@ -33,15 +35,20 @@ public class MovieDetails extends AppCompatActivity {
         TextView_movieVotes=findViewById(R.id.TextView_movieVotes);
         TextView_moviePlot=findViewById(R.id.TextView_moviePlot);
         ImageView_Poster2=findViewById(R.id.ImageView_Poster2);
-        recycler_view_cast=findViewById(R.id.recycler_view_cast);
+
+        TextView_stars=findViewById(R.id.TextView_stars);
         
         manager = new RequestManager(this);
         String movie_id=getIntent().getStringExtra("data");
         manager.searchMoviesDetails(listener,movie_id);
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Please Wait...");
+        dialog.show();
     }
     private OnDetailsApiLister listener = new OnDetailsApiLister() {
         @Override
         public void onResponse(DetailApiResponse response) {
+            dialog.dismiss();
             if (response.equals(null)){
                 Toast.makeText(MovieDetails.this, "Error Grabbing Details", Toast.LENGTH_SHORT).show();
                 return;
@@ -52,11 +59,27 @@ public class MovieDetails extends AppCompatActivity {
 
         @Override
         public void onError(String message) {
+            dialog.dismiss();
             Toast.makeText(MovieDetails.this, "Error Grabbing Details", Toast.LENGTH_SHORT).show();
 
         }
     };
 
     private void showResults(DetailApiResponse response) {
+        TextView_movieName.setText(response.getFullTitle());
+        TextView_moviePlot.setText(response.getPlot());
+        TextView_movieRating.setText(response.getImDbRating() + "⭐");
+        TextView_movieReleased.setText("Released : "+ response.getReleaseDate());
+        TextView_movieRuntime.setText(response.getRuntimeMins()+" Minutes");
+        TextView_movieVotes.setText(response.getImDbRatingVotes()+ " Votes");
+        System.out.println(response.getstars()+" Votes");
+        TextView_stars.setText("Actors: "+response.getstars());
+        try{
+            Picasso.get().load(response.getImage()).into(ImageView_Poster2);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+
     }
 }
--- app/src/main/java/com/example/imdbwatchlist/models/DetailApiResponse.java
+++ app/src/main/java/com/example/imdbwatchlist/models/DetailApiResponse.java
@@ -12,6 +12,359 @@ public class DetailApiResponse {
     String image="";
     String releaseDate="";
     String runtimeMins="";
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getFullTitle() {
+        return fullTitle;
+    }
+
+    public void setFullTitle(String fullTitle) {
+        this.fullTitle = fullTitle;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getRuntimeMins() {
+        return runtimeMins;
+    }
+
+    public void setRuntimeMins(String runtimeMins) {
+        this.runtimeMins = runtimeMins;
+    }
+
+    public String getRuntimeStr() {
+        return runtimeStr;
+    }
+
+    public void setRuntimeStr(String runtimeStr) {
+        this.runtimeStr = runtimeStr;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getPlotLocal() {
+        return plotLocal;
+    }
+
+    public void setPlotLocal(String plotLocal) {
+        this.plotLocal = plotLocal;
+    }
+
+    public Boolean getPlotLocalIsRtl() {
+        return plotLocalIsRtl;
+    }
+
+    public void setPlotLocalIsRtl(Boolean plotLocalIsRtl) {
+        this.plotLocalIsRtl = plotLocalIsRtl;
+    }
+
+    public String getAwards() {
+        return awards;
+    }
+
+    public void setAwards(String awards) {
+        this.awards = awards;
+    }
+
+    public String getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(String directors) {
+        this.directors = directors;
+    }
+
+    public List<directorList> getDirectorListList() {
+        return directorListList;
+    }
+
+    public void setDirectorListList(List<directorList> directorListList) {
+        this.directorListList = directorListList;
+    }
+
+    public String getWriters() {
+        return writers;
+    }
+
+    public void setWriters(String writers) {
+        this.writers = writers;
+    }
+
+    public List<writerList> getWriterListList() {
+        return writerListList;
+    }
+
+    public void setWriterListList(List<writerList> writerListList) {
+        this.writerListList = writerListList;
+    }
+
+    public String getstars() {
+        return stars;
+    }
+
+    public void setstars(String stars) {
+        stars = stars;
+    }
+
+    public List<starList> getStarListList() {
+        return starListList;
+    }
+
+    public void setStarListList(List<starList> starListList) {
+        this.starListList = starListList;
+    }
+
+    public List<actorList> getActorListList() {
+        return actorListList;
+    }
+
+    public void setActorListList(List<actorList> actorListList) {
+        this.actorListList = actorListList;
+    }
+
+    public Boolean getFullCast() {
+        return fullCast;
+    }
+
+    public void setFullCast(Boolean fullCast) {
+        this.fullCast = fullCast;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public List<genreList> getGenreListList() {
+        return genreListList;
+    }
+
+    public void setGenreListList(List<genreList> genreListList) {
+        this.genreListList = genreListList;
+    }
+
+    public String getCompanies() {
+        return companies;
+    }
+
+    public void setCompanies(String companies) {
+        this.companies = companies;
+    }
+
+    public List<companyList> getCompanyListList() {
+        return companyListList;
+    }
+
+    public void setCompanyListList(List<companyList> companyListList) {
+        this.companyListList = companyListList;
+    }
+
+    public String getCountries() {
+        return countries;
+    }
+
+    public void setCountries(String countries) {
+        this.countries = countries;
+    }
+
+    public List<countryList> getCountryListList() {
+        return countryListList;
+    }
+
+    public void setCountryListList(List<countryList> countryListList) {
+        this.countryListList = countryListList;
+    }
+
+    public String getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String languages) {
+        this.languages = languages;
+    }
+
+    public String getContentRating() {
+        return contentRating;
+    }
+
+    public void setContentRating(String contentRating) {
+        this.contentRating = contentRating;
+    }
+
+    public String getImDbRating() {
+        return imDbRating;
+    }
+
+    public void setImDbRating(String imDbRating) {
+        this.imDbRating = imDbRating;
+    }
+
+    public String getImDbRatingVotes() {
+        return imDbRatingVotes;
+    }
+
+    public void setImDbRatingVotes(String imDbRatingVotes) {
+        this.imDbRatingVotes = imDbRatingVotes;
+    }
+
+    public String getMetacriticRating() {
+        return metacriticRating;
+    }
+
+    public void setMetacriticRating(String metacriticRating) {
+        this.metacriticRating = metacriticRating;
+    }
+
+    public String getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(String ratings) {
+        this.ratings = ratings;
+    }
+
+    public String getWikipedia() {
+        return wikipedia;
+    }
+
+    public void setWikipedia(String wikipedia) {
+        this.wikipedia = wikipedia;
+    }
+
+    public String getPosters() {
+        return posters;
+    }
+
+    public void setPosters(String posters) {
+        this.posters = posters;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+    public String getTrailer() {
+        return trailer;
+    }
+
+    public void setTrailer(String trailer) {
+        this.trailer = trailer;
+    }
+
+    public com.example.imdbwatchlist.models.boxOffice getBoxOffice() {
+        return boxOffice;
+    }
+
+    public void setBoxOffice(com.example.imdbwatchlist.models.boxOffice boxOffice) {
+        this.boxOffice = boxOffice;
+    }
+
+    public String getTagline() {
+        return tagline;
+    }
+
+    public void setTagline(String tagline) {
+        this.tagline = tagline;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getTvSeriesInfo() {
+        return tvSeriesInfo;
+    }
+
+    public void setTvSeriesInfo(String tvSeriesInfo) {
+        this.tvSeriesInfo = tvSeriesInfo;
+    }
+
+    public String getTvEpisodeInfo() {
+        return tvEpisodeInfo;
+    }
+
+    public void setTvEpisodeInfo(String tvEpisodeInfo) {
+        this.tvEpisodeInfo = tvEpisodeInfo;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
     String runtimeStr="";
     String plot="";
     String plotLocal="";
@@ -21,7 +374,7 @@ public class DetailApiResponse {
     List<directorList> directorListList;
     String writers="";
     List<writerList> writerListList;
-    String Stars="";
+    String stars="";
     List<starList> starListList;
     List<actorList> actorListList;
     Boolean fullCast = null;
